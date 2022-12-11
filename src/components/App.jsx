96ec68d8500d81a10f7a9be335451921f6860374
Kaushal Bhardwaj <74184474+RockeTCodes@users.noreply.github.com>
2022-12-11 22:35:14 +0530

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,24 +3,17 @@ import List from "./List";
 import InputArea from "./InputArea";
 
 function App() {
-  const [input, setInput] = useState("");
   const [items, setItems] = useState([]);
 
-  function change(event) {
-    const { value } = event.target;
-    setInput(value);
-  }
-
-  function addItem() {
-    setItems((preValue) => {
-      return [...preValue, input];
+  function addItem(inputText) {
+    setItems((prevItems) => {
+      return [...prevItems, inputText];
     });
-    setInput("");
   }
 
   function deleteItem(id) {
-    setItems((preValue) => {
-      return preValue.filter((item, index) => {
+    setItems((prevItems) => {
+      return prevItems.filter((item, index) => {
         return index !== id;
       });
     });
@@ -31,16 +24,16 @@ function App() {
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
-      <div className="form">
-        <InputArea onChange={change} value={input} />
-        <button onClick={addItem}>
-          <span>Add</span>
-        </button>
-      </div>
+      <InputArea onPush={addItem} />
       <div>
         <ul>
-          {items.map((listitem, index) => (
-            <List key={index} id={index} onPush={deleteItem} value={items} />
+          {items.map((todoItem, index) => (
+            <List
+              key={index}
+              id={index}
+              text={todoItem}
+              onChange={deleteItem}
+            />
           ))}
         </ul>
       </div>
